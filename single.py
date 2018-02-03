91e869e7fa981383d97744dea6ac57bf86243001
zkt780 <ZKT780@student.bham.ac.uk>
2018-02-03 22:23:27 +0000

    test

--- single.py
+++ single.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.mixer.init()
-pygame.mixer.music.load("sounds/bass.wav")
+pygame.mixer.music.load("bass.wav")
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
     continue
