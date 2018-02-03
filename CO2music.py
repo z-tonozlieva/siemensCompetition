5dfe79f0c5d4a989a25f6b0ffc2f5bb6a9f6821c
zkt780 <ZKT780@student.bham.ac.uk>
2018-02-03 21:18:30 +0000

    adding guitar

--- CO2music.py
+++ CO2music.py
@@ -15,15 +15,16 @@ with open('5s res sensor_id=BMS-L1O14S33&start=2017-03-20T00_00_00&end=2017-03-2
 
     firstPart = false
   	secondPart = false
+    thirdPart = false
 
     def checkifComplete(channel):
         while channel.get_busy():  #Check if Channel is busy
           pygame.time.wait(800)  #  wait in ms
      channel.stop()             
 
-    if __name__ == "__main__":
-        music_file1 = "sounds/bass.wav"
-        music_file2 = "sounds/drums.wav"
+    music_file1 = "sounds/bass.wav"
+    music_file2 = "sounds/drums.wav"
+    music_file3 = "sounds/guitar.wav"
 
     freq = 44100     # audio CD quality
     bitsize = -16    # unsigned 16 bit
@@ -36,10 +37,12 @@ with open('5s res sensor_id=BMS-L1O14S33&start=2017-03-20T00_00_00&end=2017-03-2
     #Create sound object for each Audio
     myAudio1 = pygame.mixer.Sound(music_file1)
     myAudio2 = pygame.mixer.Sound(music_file2)
+    myAudio3 = pygame.mixer.Sound(music_file3)
 
     #Create a Channel for each Audio
     myChannel1 = pygame.mixer.Channel(1)
     myChannel2 = pygame.mixer.Channel(2)
+    myChannel3 = pygame.mixer.Channel(3)
     
 
     for value in values:
@@ -51,11 +54,21 @@ with open('5s res sensor_id=BMS-L1O14S33&start=2017-03-20T00_00_00&end=2017-03-2
         	if : !firstPart
         		firstPart = true
         		myChannel1.play(myAudio1)
-        		myChannel2.stop()
-        if float(value['value']) > 450.0 :
+        	myChannel2.stop()
+            secondPart = false
+            myChannel3.stop()
+            thirdPart = false
+        elif float(value['value']) > 450.0 && float(value['value']) < 600.0 :
         	if : !secondPart
         		secondPart = true
         		myChannel2.play(myAudio2)
+            myChannel3.stop()
+            thirdPart = false
+        else 
+            if : !thirdPart
+                thirdPart = true
+                myChannel3.play(myAudio3)
+
         print(value['value'])
         time.sleep(10)
         	
