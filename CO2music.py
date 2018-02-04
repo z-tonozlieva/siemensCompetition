6284d1b974c28e38f6feced41dc535bc61918cd9
zkt780 <ZKT780@student.bham.ac.uk>
2018-02-04 09:16:40 +0000

    fixing

--- CO2music.py
+++ CO2music.py
@@ -6,74 +6,143 @@ with open('5s res sensor_id=BMS-L1O14S33&start=2017-03-20T00_00_00&end=2017-03-2
     d = json.load(json_data)
     data = d['id']
     name = d['name']
+    type = d['type']
     values = d['values']
+    print(name)
+    print(type)
+    print("---------")
 
     firstPart = False
     secondPart = False
     thirdPart = False
+    fourthPart = False
+    fifthPart = False
+    sixthPart = False
 
-def checkifComplete(channel):
-    while channel.get_busy():  #Check if Channel is busy
-        pygame.time.wait(800)  #  wait in ms
-    channel.stop()             #Stop channel
 
 if __name__ == "__main__":
-    music_file1 = "guitar.wav"
-    music_file2 = "drums.wav"
-    music_file3 = "bass.wav"
-    music_file11 = "1-Morning-Edvard-Grieg.wav"
-    music_file22 = "2-Erik-Satie-Gymnop_die-No_1.wav"
-    music_file33 = "3-Camille-Saint-Sa_ns-Danse-Macabre.wav"
+    music_file1 = "1-Morning-Edvard-Grieg.wav"
+    music_file2 = "2-Erik-Satie-Gymnop_die-No_1.wav"
+    music_file3 = "3-Camille-Saint-Sa_ns-Danse-Macabre.wav"
     music_file4 = "4-Richard-Wagner-Ride-Of-The-Valkyries.wav"
     music_file5 = "5-Mozart-The-Marriage-of-Figaro.wav"
     music_file6 = "6-Grieg-In-the-Hall-of-the-Mountain-King.wav"
 
 # set up the mixer
-freq = 44100  # audio CD quality
-bitsize = -16  # unsigned 16 bit
-channels = 3  # 1 is mono, 2 is stereo
-buffer = 2048  # number of samples (experiment to get right sound)
+freq = 44100
+bitsize = -16
+channels = 6
+buffer = 2048
 pygame.mixer.init(freq, bitsize, channels, buffer)
 
-pygame.mixer.init()  # Initialize Mixer
+pygame.mixer.init()
 
 # Create sound object for each Audio
 myAudio1 = pygame.mixer.Sound(music_file1)
 myAudio2 = pygame.mixer.Sound(music_file2)
 myAudio3 = pygame.mixer.Sound(music_file3)
+myAudio4 = pygame.mixer.Sound(music_file4)
+myAudio5 = pygame.mixer.Sound(music_file5)
+myAudio6 = pygame.mixer.Sound(music_file6)
 
 # Create a Channel for each Audio
 myChannel1 = pygame.mixer.Channel(1)
 myChannel2 = pygame.mixer.Channel(2)
 myChannel3 = pygame.mixer.Channel(3)
+myChannel4 = pygame.mixer.Channel(4)
+myChannel5 = pygame.mixer.Channel(5)
+myChannel6 = pygame.mixer.Channel(6)
 
 for value in values:
-    if float(value['value']) <= 500.0:
+    if float(value['value']) <= 400.0:
         myChannel2.stop()
         secondPart = False
         myChannel3.stop()
         thirdPart = False
+        myChannel4.stop()
+        fourthPart = False
+        myChannel5.stop()
+        fifthPart = False
+        myChannel6.stop()
+        sixthPart = False
         if not firstPart or not myChannel1.get_busy():
             firstPart = True
-            "Playing audio : ", music_file1
             myChannel1.play(myAudio1)
 
-    elif (float(value['value']) > 500.0) and (float(value['value']) < 600.0) :
+    elif (float(value['value']) > 400.0) and (float(value['value']) <= 500.0) :
+        myChannel1.stop()
+        firstPart = False
         myChannel3.stop()
         thirdPart = False
+        myChannel4.stop()
+        fourthPart = False
+        myChannel5.stop()
+        fifthPart = False
+        myChannel6.stop()
+        sixthPart = False
         if not secondPart or not myChannel2.get_busy() :
             secondPart = True
-            "Playing audio : ", music_file2
             myChannel2.play(myAudio2)
 
-    else:
+    elif (float(value['value']) > 500.0) and (float(value['value']) <= 600.0) :
+        myChannel1.stop()
+        firstPart = False
+        myChannel2.stop()
+        secondPart = False
+        myChannel4.stop()
+        fourthPart = False
+        myChannel5.stop()
+        fifthPart = False
+        myChannel6.stop()
+        sixthPart = False
         if not thirdPart or not myChannel3.get_busy() :
             thirdPart = True
-            "Playing audio : ", music_file3
             myChannel3.play(myAudio3)
+
+    elif (float(value['value']) > 600.0) and (float(value['value']) <= 700.0) :
+        myChannel1.stop()
+        firstPart = False
+        myChannel2.stop()
+        secondPart = False
+        myChannel3.stop()
+        thirdPart = False
+        myChannel5.stop()
+        fifthPart = False
+        myChannel6.stop()
+        sixthPart = False
+        if not fourthPart or not myChannel4.get_busy() :
+            fourthPart = True
+            myChannel4.play(myAudio4)
+
+    elif (float(value['value']) > 700.0) and (float(value['value']) <= 800.0) :
+        myChannel1.stop()
+        firstPart = False
+        myChannel2.stop()
+        secondPart = False
+        myChannel3.stop()
+        thirdPart = False
+        myChannel4.stop()
+        fourthPart = False
+        myChannel6.stop()
+        sixthPart = False
+        if not fifthPart or not myChannel5.get_busy() :
+            fifthPart = True
+            myChannel5.play(myAudio5)
+
+    else:
+        myChannel1.stop()
+        firstPart = False
+        myChannel2.stop()
+        secondPart = False
+        myChannel3.stop()
+        thirdPart = False
+        myChannel4.stop()
+        fourthPart = False
+        myChannel5.stop()
+        fifthPart = False
+        if not sixthPart or not myChannel6.get_busy() :
+            sixthPart = True
+            myChannel6.play(myAudio6)
 
     print(value['value'])
     time.sleep(5)
-
-print("---------")
-print(name)
